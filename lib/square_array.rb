af6243e67dcf3a93e28ecf467d5cd12dbc798777
delightful-source-3050 <joshmrallen@gmail.com>
2020-02-24 16:11:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   squared = []
   
-  while (counter < array.length) do
+  while counter < array.length do
     squared[counter] = array[counter] * array[counter]
     counter+=1
   end
